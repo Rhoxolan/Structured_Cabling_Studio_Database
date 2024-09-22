c873d9877bf5992ef907c193239dbb824688e814
Rhoxolan <pba17mail@gmail.com>
2024-09-23 00:18:19 +0300

    Began work on the CalculateStructuredCablingConfiguration procedure; some other tech changes

---
+++ Structured Cabling Studio Database/Procedures/CalculateStructuredCablingConfiguration.sql
@@ -0,0 +1,3 @@
+CREATE PROCEDURE CalculateStructuredCablingConfiguration
+    @CalculateViewModel XML INPUT
+    @CablingConfiguration XML OUTPUT
--- Structured Cabling Studio Database/Scripts/InitializeDatabase_sample.sql
+++ Structured Cabling Studio Database/Scripts/InitializeDatabase_sample.sql
@@ -1,2 +1,6 @@
+-- Tables
 :R "..\Scripts\IdentityInitialize.sql"
 :R "..\Tables\CablingConfigurations.sql"
+
+--Procedures
+:R "..\Procedures\SetAuthorizationSessionContext.sql"
