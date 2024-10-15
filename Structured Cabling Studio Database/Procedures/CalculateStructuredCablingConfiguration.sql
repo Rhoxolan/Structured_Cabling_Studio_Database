9c714b0bd02975d9e19c3de3c376d9869da9a24b
Rhoxolan <pba17mail@gmail.com>
2024-10-16 00:16:37 +0300

    Changes in the CalculateStructuredCablingConfiguration procedure

--- Structured Cabling Studio Database/Procedures/CalculateStructuredCablingConfiguration.sql
+++ Structured Cabling Studio Database/Procedures/CalculateStructuredCablingConfiguration.sql
@@ -1,4 +1,14 @@
 CREATE PROCEDURE CalculateStructuredCablingConfiguration
-    @StructuredCablingStudioParameters XML INPUT
     @ConfigurationCalculateParameters XML INPUT
+    @StructuredCablingStudioParameters XML INPUT
     @CablingConfiguration XML OUTPUT
+AS
+BEGIN
+    DECLARE @CableHankMeterage INT;
+    DECLARE @IsCableHankMeterageAvailability BIT;
+
+    DECLARE @TechnologicalReserve FLOAT;
+    DECLARE @IsRecommendationsAvailability BIT;
+    DECLARE @IsStrictComplianceWithTheStandart BIT;
+    DECLARE @IsAnArbitraryNumberOfPorts BIT;
+    DECLARE @IsTechnologicalReserveAvailability BIT;
