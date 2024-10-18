22d1b793e992523a0cb865852786aa7474988883
Rhoxolan <pba17mail@gmail.com>
2024-10-18 22:53:18 +0300

    The CalculateStructuredCablingConfiguration procedure was supplemented

--- Structured Cabling Studio Database/Procedures/CalculateStructuredCablingConfiguration.sql
+++ Structured Cabling Studio Database/Procedures/CalculateStructuredCablingConfiguration.sql
@@ -18,6 +18,15 @@ BEGIN
     DECLARE @IsAnArbitraryNumberOfPorts BIT;
     DECLARE @IsTechnologicalReserveAvailability BIT;
 
+    SET @CableHankMeterage = @ConfigurationCalculateParameters.value('(/ConfigurationCalculateParameters/CableHankMeterage)[1]', 'int');
+    SET @IsCableHankMeterageAvailability = @ConfigurationCalculateParameters.value('(/ConfigurationCalculateParameters/IsCableHankMeterageAvailability)[1]', 'bit');
+
+    SET @TechnologicalReserve = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/TechnologicalReserve)[1]', 'float');
+    SET @IsRecommendationsAvailability = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsRecommendationsAvailability)[1]', 'bit');
+    SET @IsStrictComplianceWithTheStandart = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsStrictComplianceWithTheStandart)[1]', 'bit');
+    SET @IsAnArbitraryNumberOfPorts = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsAnArbitraryNumberOfPorts)[1]', 'bit');
+    SET @IsTechnologicalReserveAvailability = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsTechnologicalReserveAvailability)[1]', 'bit');
+
     SET @CablingConfiguration = CASE
                                     WHEN @IsCableHankMeterageAvailability = 1
                                         THEN CalculateConfigurationWithHankMeterage(@MinPermanentLink,
@@ -40,8 +49,7 @@ BEGIN
                                                                                     @IsRecommendationsAvailability,
                                                                                     @IsStrictComplianceWithTheStandart,
                                                                                     @IsAnArbitraryNumberOfPorts,
-                                                                                    @IsTechnologicalReserveAvailability
-                                                                                    )
+                                                                                    @IsTechnologicalReserveAvailability)
                                 END;
 
 END;
