c3a867ba753bd298a243e923a0ac8b565229e257
Rhoxolan <pba17mail@gmail.com>
2024-12-05 00:29:33 +0200

    Began work on the SetStructuredCablingStudioParametersDiapasons procedure

---
+++ Structured Cabling Studio Database/Calculation/Procedures/SetStructuredCablingStudioParametersDiapasons.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE Calculation.SetStructuredCablingStudioParametersDiapasons
+    @StructuredCablingStudioParameters XML
+AS
+BEGIN
+    DECLARE @IsRecommendationsAvailability BIT;
+    DECLARE @IsStrictComplianceWithTheStandart BIT;
+    DECLARE @IsAnArbitraryNumberOfPorts BIT;
+    DECLARE @IsTechnologicalReserveAvailability BIT;
+
+    DECLARE @StructuredCablingStudioDiapasons XML;
+
+    SET @IsRecommendationsAvailability = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsRecommendationsAvailability)[1]', 'bit');
+    SET @IsStrictComplianceWithTheStandart = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsStrictComplianceWithTheStandart)[1]', 'bit');
+    SET @IsAnArbitraryNumberOfPorts = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsAnArbitraryNumberOfPorts)[1]', 'bit');
+    SET @IsTechnologicalReserveAvailability = @StructuredCablingStudioParameters.value('(/StructuredCablingStudioParameters/IsTechnologicalReserveAvailability)[1]', 'bit');
+
+    SELECT @StructuredCablingStudioDiapasons = Calculate.GetStructuredCablingStudioParametersDiapasons(@IsRecommendationsAvailability,
+                                                                                                    @IsStrictComplianceWithTheStandart,
+                                                                                                    @IsAnArbitraryNumberOfPorts,
+                                                                                                    @IsTechnologicalReserveAvailability);
+
+END;
