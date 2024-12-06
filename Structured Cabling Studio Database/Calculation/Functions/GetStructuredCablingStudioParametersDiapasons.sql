eff134dacb17e234631ff3266a27a354b34c586c
Rhoxolan <pba17mail@gmail.com>
2024-12-07 00:37:36 +0200

    Work on the GetStructuredCablingStudioParametersDiapasons function

--- Structured Cabling Studio Database/Calculation/Functions/GetStructuredCablingStudioParametersDiapasons.sql
+++ Structured Cabling Studio Database/Calculation/Functions/GetStructuredCablingStudioParametersDiapasons.sql
@@ -9,5 +9,19 @@ AS
 BEGIN
     DECLARE @StructuredCablingStudioDiapasons XML;
 
+    DECLARE @MinPermanentLinkDiapason XML;
+    DECLARE @MaxPermanentLinkDiapason XML;
+    DECLARE @NumberOfPortsDiapason XML;
+    DECLARE @NumberOfWorkplacesDiapason XML;
+    DECLARE @CableHankMeterageDiapason XML;
+    DECLARE @TechnologicalReserveDiapason XML;
+
+    SELECT @MinPermanentLinkDiapason = Calculate.GetMinPermanentLinkDiapason(@IsStrictComplianceWithTheStandart);
+    SELECT @MaxPermanentLinkDiapason = Calculate.GetMaxPermanentLinkDiapason(@IsStrictComplianceWithTheStandart);
+    SELECT @NumberOfPortsDiapason = Calculate.GetNumberOfPortsDiapason(@IsAnArbitraryNumberOfPorts);
+    SELECT @NumberOfWorkplacesDiapason = Calculate.GetNumberOfWorkplacesDiapason(@IsStrictComplianceWithTheStandart);
+    SELECT @CableHankMeterageDiapason = Calculate.GetCableHankMeterageDiapason(@IsStrictComplianceWithTheStandart);
+    SELECT @TechnologicalReserveDiapason = Calculate.GetTechnologicalReserveDiapason(@IsTechnologicalReserveAvailability);
+
 
 END;
