5347fff499675b86263b914487513c3b3e1b6c81
Rhoxolan <pba17mail@gmail.com>
2024-12-08 19:55:08 +0200

    Completed SeedDiapasons script

--- Structured Cabling Studio Database/Scripts/SeedDiapasons.sql
+++ Structured Cabling Studio Database/Scripts/SeedDiapasons.sql
@@ -1,20 +1,20 @@
 INSERT INTO Calculate.CableHankMeterageDiapasons VALUES
-(0, 0)
+(0.01, 10000)
 
 INSERT INTO Calculate.MaxPermanentLinkDiapasons VALUES
-(0, 0, TRUE),
-(0, 0, FALSE)
+(1, 90, TRUE),
+(0.01, 1000, FALSE)
 
 INSERT INTO Calculate.MinPermanentLinkDiapasons VALUES
-(0, 0, TRUE),
-(0, 0, FALSE)
+(1, 90, TRUE),
+(0.01, 1000, FALSE)
 
 INSERT INTO Calculate.NumberOfPortsDiapasons VALUES
-(0, 0, TRUE),
-(0, 0, FALSE)
+(1, 100, TRUE),
+(2, 100, FALSE)
 
 INSERT INTO Calculate.TechnologicalReserveDiapasons VALUES
-(0, 0)
+(1, 2)
 
 INSERT INTO Calculate.NumberOfWorkplacesDiapasons VALUES
-(0, 0)
+(1, 10000)
